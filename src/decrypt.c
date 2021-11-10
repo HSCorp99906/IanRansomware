6800abe522035eb7d7019db65733f9672593b216
Ian <teaqllabs@gmail.com>
2021-11-10 12:14:01 -0500

    First Commit.

---
+++ src/decrypt.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+#include <stdlib.h>
+
+
+int main() {
+    DIR* dir = opendir("../Important/");
+    struct dirent* en;
+
+    if (dir != NULL) {
+        while ((en = readdir(dir)) != NULL) {
+            if (!(strstr(en -> d_name, "."))) {
+                char nameBuf[256] = "../Important/";
+                strncat(nameBuf, en -> d_name, 30);
+
+                printf("Decrypting: %s\n", en -> d_name);
+
+                char fcBuffer[5000];
+
+                FILE* fp = fopen(nameBuf, "r");
+
+                fseek(fp, 0, SEEK_END);
+                size_t size = ftell(fp);
+                fseek(fp, 0, SEEK_SET);
+
+                fread(fcBuffer, 1, size, fp);
+
+                char writeBuffer[5000];
+
+                for (int i = 0; i < size; ++i) {
+                    writeBuffer[i] = fcBuffer[i] - 100;
+                }
+
+                FILE* fp1 = fopen(nameBuf, "w");
+
+                fputs(writeBuffer, fp1);
+
+                fclose(fp1);
+                fclose(fp);
+            }
+        }
+
+        printf("\nYour files have been decrypted.");
+        closedir(dir);
+    }
+}
